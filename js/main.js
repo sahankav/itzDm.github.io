0e8897fd6cbb39fda133521f6e4f862705c4b0fa
itzDM <bitdev20@gmail.com>
2024-01-11 14:57:21 +0530

    about page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Dark Mode
 const mode = localStorage.getItem("mode") || "";
 const toggle = document.querySelector(".toggle");
 const body = document.querySelector(" body");
@@ -8,3 +9,4 @@ toggle.addEventListener("click", () => {
   localStorage.setItem("mode", mode == "light" ? "" : "light");
   body.classList.toggle("light");
 });
+
